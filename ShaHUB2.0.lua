63440db9411644ef5a5e44000f5d7716990dd473
114514lzkill <3957336350@qq.com>
2025-08-15 12:19:29 +0800

    Update ShaHUB2.0.lua

--- ShaHUB2.0.lua
+++ ShaHUB2.0.lua
@@ -1,1 +1,40 @@
-404
+
+
+print("Anti Afk On")
+        local vu = game:GetService("VirtualUser")
+        game:GetService("Players").LocalPlayer.Idled:connect(function()
+           vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+           wait(1)
+           vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+        end)
+      wait(1)
+        local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "反挂机2已开启",
+    Text = "虽然不知道有没有增强",
+    Duration = 5,
+})
+
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+local Window = WindUI:CreateWindow({
+    Title = "沙脚本测试版",
+    Icon = "14895392107",
+    Author = "Example UI",
+    Folder = "MyTestHub",
+})
+
+local Tab = Window:Tab({
+    Title = "通用",
+    Icon = "14895392107",
+    Locked = false,
+})
+
+local Button = Tab:Button({
+    Title = "飞行",
+    Desc = "Test Button",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/114514lzkill/-/refs/heads/main/%E9%A3%9E%E8%A1%8C%E8%84%9A%E6%9C%AC.lua"))()
+    end
+})
